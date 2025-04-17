fcad30aeb02fec4a299853e5d3265efecbfd9906
DeaThGuNNNN <mohamedriyadbadou@gmail.com>
2025-04-17 08:06:04 +0200

    Initial commit: AWS Dashboard with table selection fix

---
+++ lib/mock-data.ts
@@ -0,0 +1,7 @@
+// Import data directly from JSON files
+import ec2Data from "../data/EC2_Merged.json"
+import rdsData from "../data/RDS_Merged.json"
+
+// Export the data
+export const mockEC2Data = ec2Data
+export const mockRDSData = rdsData
